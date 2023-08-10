c6a4d384c01f0b417b2d0856e94d43aa53e0916b
umamuthu94 <umamuthu1994@gmail.com>
2023-08-10 12:17:44 +0530

    we add this for creating a new branch2 for understating merge concepts

--- src/main/java/com/example/practice/model/Employee.java
+++ src/main/java/com/example/practice/model/Employee.java
@@ -1,5 +1,9 @@
 package com.example.practice.model;
 
+
+@Entity
 public class Employee {
-    
+    @Id
+    private Integer id;//we add this for creating a new branch2
+    private String name;//branch2
 }
