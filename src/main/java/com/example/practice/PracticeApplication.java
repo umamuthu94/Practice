db753de70851c1a4ebaa804552de010ed164e2f7
durga1808 <durgalskshmi3713@gmail.com>
2023-08-10 14:39:01 +0530

    added the branch 7

--- src/main/java/com/example/practice/PracticeApplication.java
+++ src/main/java/com/example/practice/PracticeApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PracticeApplication {
-
+        
 	public static void main(String[] args) {
-		SpringApplication.run(PracticeApplication.class, args);
+		SpringApplication.run(PracticeApplication.class, args);//initialize the arguments
+		System.out.println(args);//printing the aruguments create the branch7 added in the repo.
 	}
 
 }
