5641850cbd60ee8c3d0d834f579b0b3bd44710d1
durga1808 <durgalskshmi3713@gmail.com>
2023-08-10 16:50:57 +0530

    deleted the commit changes

--- src/main/java/com/example/practice/PracticeApplication.java
+++ src/main/java/com/example/practice/PracticeApplication.java
@@ -3,7 +3,7 @@ package com.example.practice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@
 public class PracticeApplication {
         
 	public static void main(String[] args) {
@@ -12,3 +12,4 @@ public class PracticeApplication {
 	}
 
 }
+SpringBootApplication
