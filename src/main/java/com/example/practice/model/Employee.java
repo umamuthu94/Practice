fcdcea705b4b448e2c073f606369eaf7d49a96a3
umamuthu94 <umamuthu1994@gmail.com>
2023-08-10 12:24:14 +0530

    we creating a new branch3 and these branch shows 3 lines we deleted for these operations understanding merge

--- src/main/java/com/example/practice/model/Employee.java
+++ src/main/java/com/example/practice/model/Employee.java
@@ -1,9 +1,14 @@
 package com.example.practice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
+@Data//we creating a new branch3 and these branch shows 3 lines we deleted
+@AllArgsConstructor//branch3
+@NoArgsConstructor//branch3
 public class Employee {
-    @Id
+    
     private Integer id;//we add this for creating a new branch2
     private String name;//branch2
 }
